523f476598080ff8536f9343ba78538fada19ebc
pro_rokky <vlad6005@gmail.com>
2022-11-09 23:41:22 +0300

    fix loading with webpack

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,11 @@ import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import React, { useEffect } from 'react'
 
-//webpack failing when loading leaflet marker icon
 delete (L.Icon.Default as any).prototype._getIconUrl
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-  iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+  iconRetinaUrl: require('./assets/marker-icon-2x.png').default,
+  iconUrl: require('./assets/marker-icon.png').default,
+  shadowUrl: require('./assets/marker-shadow.png').default,
 })
 
 type ClusterType = { [key in string]: any }
